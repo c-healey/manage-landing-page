179c486f65bc983547928a938dd2cec4190446af
Cathy <cathy.healey@gmail.com>
2020-05-21 10:49:24 -0400

    Updates

---
+++ precache-manifest.ebcd97cd86b1e89b96a40fe2d10372d4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0f1dfa1f404fea2b18f6c002c69a39e",
+    "url": "/manage-landing-page/index.html"
+  },
+  {
+    "revision": "ec5cd0265507412776a3",
+    "url": "/manage-landing-page/static/css/main.d53ab144.chunk.css"
+  },
+  {
+    "revision": "ea4a43267a9fe036e095",
+    "url": "/manage-landing-page/static/js/2.e5b27a1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/manage-landing-page/static/js/2.e5b27a1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec5cd0265507412776a3",
+    "url": "/manage-landing-page/static/js/main.97aef91e.chunk.js"
+  },
+  {
+    "revision": "759d068e2c7e79210bff",
+    "url": "/manage-landing-page/static/js/runtime-main.8d6120f1.js"
+  },
+  {
+    "revision": "d6d0cb363a1a3801a691e7562e2911d2",
+    "url": "/manage-landing-page/static/media/bg-simplify-section-desktop.d6d0cb36.svg"
+  },
+  {
+    "revision": "37dc03724048fd8cfc2681353ff9c84a",
+    "url": "/manage-landing-page/static/media/bg-simplify-section-mobile.37dc0372.svg"
+  },
+  {
+    "revision": "010617910cf89bbbf2df1f8bf2340912",
+    "url": "/manage-landing-page/static/media/bg-tablet-pattern.01061791.svg"
+  }
+]);
